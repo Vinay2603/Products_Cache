8728843f92cf50819ceab44c9cc00f607294e5e6
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2021-12-12 02:24:55 +0530

    add

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -5,21 +5,6 @@ const Product = require("../models/product")
 const router = express.Router()
 const redis = require("../configs/redis")
 
-router.post("/", async (req, res )=>{
-    try{
-    console.log('req:', req.body)
-    const product = await Product.create(req.body)
-    console.log('product:', product)
-   // const products = await Product.find({}).exec()
-   // console.log('products:', products)
-   // redis.set("product", JSON.stringify(products))
-
-    return res.status(201).send(product)
-
-    }catch(e){
-        return res.status(500).json( {message: e.message , status : "failed"})
-    }
-})
 
 
 
@@ -42,6 +27,65 @@ router.get("/", (req,res)=>{
        })
  
 })
+
+
+router.post("/", async (req, res )=>{
+
+    const product = await Product.create(req.body)
+
+    
+
+    const products = await Product.find({}).exec()
+
+
+    redis.set("product", JSON.stringify(products))
+
+    return res.status(201).send(product)
+
+   
+})
+
+
+router.get("/:id", (req,res)=>{
+    if(err) console.error(err)
+
+    redis.get(`product.${req.params.id}`, async(err, pros)=>{
+        if(pros) return res.status(200).send({cached_pros : JSON.parse(pros)})
+
+        const product =  await Product.findById(req.params.id).lean().exec()
+
+        redis.set(`product.${req.params.id}`, JSON.stringify(product))
+
+        return res.status(200).send({db_pros : product})
+    })
+})
+
+router.patch("/:id", async(req,res)=>{
+            const product = await Product.findByIdAndUpdate(res.params.id, req.body,{new: true})
+
+            redis.set(`product.${req.params.id}`, JSON.stringify(product))
+
+            const products = await Product.find().exec()
+
+            redis.set("products", JSON.stringify(products))
+
+            return res.status(201).send(product)
+
+})
+
+router.delete("/:id", async(req,res)=>{
+    const product = await Product.findByIdAndDelete(req.params.id)
+
+    redis.del(`product.${req.params.id}`)
+
+    const products = await Product.find().exec()
+
+    redis.set("products", JSON.stringify(products))
+
+    return res.status(201).send(product)
+})
+
+
 
 
 module.exports = router
