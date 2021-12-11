f3ae2c221e7a8186fdb694678bd9a7dc956d25eb
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2021-12-11 18:52:41 +0530

    add

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -5,21 +5,43 @@ const Product = require("../models/product")
 const router = express.Router()
 const redis = require("../configs/redis")
 
+router.post("/", async (req, res )=>{
+    try{
+    console.log('req:', req.body)
+    const product = await Product.create(req.body)
+    console.log('product:', product)
+   // const products = await Product.find({}).exec()
+   // console.log('products:', products)
+   // redis.set("product", JSON.stringify(products))
+
+    return res.status(201).send(product)
+
+    }catch(e){
+        return res.status(500).json( {message: e.message , status : "failed"})
+    }
+})
+
+
+
+
 router.get("/", (req,res)=>{
 
        redis.get("products",async function(err, pros){
             console.log('pros:', pros)
             if(err) console.log(err)
 
-            if(pros) return res.status(200).send(pros)
+            if(pros) return res.status(200).send({ pros : JSON.parse(pros)})
+            
+            console.log("reached here" )
 
             const product = await Product.find({}).lean().exec()
+ 
+            redis.set("product", JSON.stringify(product))
 
-            return res.status(200).send(product)
+            return res.status(200).send({ pros1 : product})
        })
  
-
-   
 })
+
 
 module.exports = router
--- src/models/product.js
+++ src/models/product.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 
-const productSchema = new mongoose.Schema({
-    name :{type: String , required: true },
-    price :{type: Number , required: true },
+const productSchema= new mongoose.Schema({
+    name:{type:String , required: true },
+    price:{type: String , required: true }
 },{
-    versionKey: false ,
-    timestamps: true ,
+    versionKey:false,
+    timestamps: true 
 })
 
-module.exports = mongoose.model("products" , productSchema)
+module.exports= mongoose.model("product", productSchema)
 
